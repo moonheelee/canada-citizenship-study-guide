32b1d4d6c4e56a9b815fa8cd39262697d4447713
Moonhee Lee <moonhee.lee@fourseasonsoft.com>
2023-12-10 16:56:43 -0800

    Improve code readability and understandability
    This commit removes unnecessary print statements used for debugging and enhances the code legibility by inserting explanatory comments. The comments shed light on various process stages, such as handling user input, processing the assistant operations, and rendering the assistant's response, making the code more comprehensible and easier to maintain.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 from openai import OpenAI
 
-
+# Function to display each line of the message
 def write_message_content(message):
     with st.chat_message(message.role):
         for line in message.content[0].text.value.splitlines():
@@ -14,27 +14,25 @@ def main():
     st.header("Canada Citizenship Study Guide")
 
     load_dotenv()
-
     client = OpenAI(api_key=os.getenv("OPENAI_API"))
 
+    # Retrieve or create assistant and thread IDs
     if 'assistant_id' not in st.session_state:
         assistant = client.beta.assistants.retrieve(os.getenv("ASSISTANT_ID"))
-        print(f"Retrieve assistant: {assistant.id}")
         st.session_state.assistant_id = assistant.id
-
     if 'thread_id' not in st.session_state:
         thread = client.beta.threads.create()
-        print(f"Create a new thread: {thread.id}")
         st.session_state.thread_id = thread.id
 
     assistant_id = st.session_state.assistant_id
     thread_id = st.session_state.thread_id
 
+    # Display all messages in the thread
     thread_messages = client.beta.threads.messages.list(thread_id, order="asc")
-
     for message in thread_messages.data:
         write_message_content(message)
 
+    # Process user input and display it
     prompt = st.chat_input("Let's get started!")
     if prompt:
         message = client.beta.threads.messages.create(
@@ -44,20 +42,20 @@ def main():
         )
         write_message_content(message)
 
+        # Start the assistant's processing and wait for completion
         run = client.beta.threads.runs.create(
             thread_id=thread_id,
             assistant_id=assistant_id
         )
-
         with st.spinner("Wait for it..."):
             while run.status != "completed":
                 time.sleep(1)
-
                 run = client.beta.threads.runs.retrieve(
                     thread_id=thread_id,
                     run_id=run.id
                 )
 
+        # Display the assistant's response
         messages = client.beta.threads.messages.list(thread_id)
         write_message_content(messages.data[0])
 
